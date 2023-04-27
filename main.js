8bfd45626d020b6317736fe3cc44bfd3a8c6ecfa
yokesh-code <114916680+yokesh-code@users.noreply.github.com>
2023-04-27 14:40:06 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,34 @@
+    const time=document.querySelector(".time");
+    setTimeout(function (){
+        time.innerText="10"
+        setTimeout(function (){
+        time.innerText="9"
+        setTimeout(function (){
+        time.innerText="8"
+        setTimeout(function (){
+        time.innerText="7"
+        setTimeout(function (){
+        time.innerText="6"
+        setTimeout(function (){
+        time.innerText="5"
+        setTimeout(function (){
+        time.innerText="4"
+        setTimeout(function (){
+        time.innerText="3"
+        setTimeout(function (){
+        time.innerText="2"
+        setTimeout(function (){
+        time.innerText="1"
+        setTimeout(function (){
+        time.innerText="Happy Independence Day"
+        },1000)
+        },1000)
+        },1000)
+        },1000)
+        },1000)
+        },1000)
+        },1000)
+        },1000)
+        },1000)
+        },1000)
+    },1000)
